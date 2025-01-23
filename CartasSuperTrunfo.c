f287e5d06e67231071422957b291700106fec7fb
GabrielAnverse <gabrielmacielcpt@gmail.com>
2025-01-23 13:36:39 +0000

    Mudei poucos detalhes para melhorar a visualização

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -21,7 +21,10 @@ int main() {
     char codigo[4], nome[20];
     int populacao, pontosturisticos;
     float area;
-    double pib;
+    float pib;
+
+    printf("Bem-vindo ao cadastro de cartas do Jogo Super Trunfo - Países!\n");
+    printf("Você irá cadastrar 32 cidades, divididas em 8 estados (A a H).\n\n");
 
     printf("Digite o nome da cidade:\n ");
     scanf("%s", &nome); 
@@ -35,12 +38,15 @@ int main() {
     scanf("%f", &area);
     printf("Digite o Pib:\n ");
     scanf("%f", &pib);
+
+    printf("--------------------------------\n");
     printf("Nome da cidade: %s\n", nome);
-    printf("código: %s\n", codigo);
+    printf("Código: %s\n", codigo);
     printf("População: %d\n", populacao);
     printf("Pontos turisticos: %d\n", pontosturisticos);
     printf("Área: %f\n", area);
     printf("Pib: %f\n", pib);
+    printf("--------------------------------\n");
 
 
 
